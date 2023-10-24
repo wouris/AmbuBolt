e62380ccb9cba383daaf4bfb23e9add3f0a4ac17
RichardMacaj <richardmacaj@kosickaakademia.sk>
2023-10-24 20:05:07 +0200

    pray

---
+++ src/app/incident-details/incident-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incident-details',
+  templateUrl: './incident-details.component.html',
+  styleUrls: ['./incident-details.component.css']
+})
+export class IncidentDetailsComponent {
+
+}
